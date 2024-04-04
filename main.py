6cf8cbe977d662ef10c53c4293d43a46393811c7
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-04-04 11:50:44 +0500

    fix: chain

--- main.py
+++ main.py
@@ -1,64 +1,59 @@
 import os
 
 from langchain import hub
+
 from langchain_openai import ChatOpenAI
 from langchain_openai.embeddings import OpenAIEmbeddings
+
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+from langchain.text_splitter import CharacterTextSplitter
+
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 
 from dotenv import load_dotenv
 
-# load env from local .env file
-load_dotenv()
+
+load_dotenv(".env")
 
 
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
 
-def main() -> None:
-    # load .txt file
-    loader = TextLoader("media/example.txt")
-    documents = loader.load()
+def get_chain():
+    data = TextLoader("example.txt")
+    docs = data.load()
 
-    # split text
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=100, chunk_overlap=20, length_function=len, is_separator_regex=False
+    text_splitter = CharacterTextSplitter(
+        chunk_size=100, chunk_overlap=20, length_function=len
     )
-    splits = text_splitter.split_documents(documents)
+    text_splits = text_splitter.split_documents(docs)
 
-    # create embeddings
     embeddings = OpenAIEmbeddings()
-
-    vectorstore = None
-    if os.path.exists("vectordb/"):
-        vectorstore = FAISS.load_local(f"vectordb/", embeddings)
-    else:
-        vectorstore = FAISS.from_documents(splits, embeddings)
-        vectorstore.save_local("vectordb/")
-
+    vectorstore = FAISS.from_documents(text_splits, embeddings)
     retriever = vectorstore.as_retriever()
 
-    query = {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    prompt = hub.pull("rlm/rag-prompt")
-    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.8)
+    query = {
+        "context": retriever | format_docs,
+        "question": RunnablePassthrough(),
+    }
+    llm = ChatOpenAI(
+        model="gpt-3.5-turbo",
+        temperature=0.8,
+    )
+    rag_prompt = hub.pull("rlm/rag-prompt")
     oputput_parser = StrOutputParser()
 
-    # create rag chain
-    chain = query | prompt | llm | oputput_parser
+    chain = query | rag_prompt | llm | oputput_parser
 
-    while True:
-        question = input("Enter your question: ")
+    return chain
 
-        print("\nProcessing your query...\n")
 
-        response = chain.invoke(question)
-
-        print("ChatGPT ==>", response)
-        print()
+def main() -> None:
+    pass
 
 
 if __name__ == "__main__":

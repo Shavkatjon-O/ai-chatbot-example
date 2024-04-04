19c47e0b79d48b678e7c3be08aa08c5c91922a70
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-04-04 12:24:05 +0500

    feat: gradio interface

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import TextLoader
 
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
@@ -19,22 +19,18 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 
 
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-
-
 def get_chain():
     data = TextLoader("example.txt")
     docs = data.load()
 
-    text_splitter = CharacterTextSplitter(
-        chunk_size=100, chunk_overlap=20, length_function=len
-    )
+    text_splitter = RecursiveCharacterTextSplitter()
     text_splits = text_splitter.split_documents(docs)
 
     embeddings = OpenAIEmbeddings()
     vectorstore = FAISS.from_documents(text_splits, embeddings)
     retriever = vectorstore.as_retriever()
+
+    format_docs = lambda docs: "\n\n".join(doc.page_content for doc in docs)
 
     query = {
         "context": retriever | format_docs,
@@ -53,7 +49,14 @@ def get_chain():
 
 
 def main() -> None:
-    pass
+    chain = get_chain()
+
+    get_answer = lambda question, history: chain.invoke(question)
+
+    import gradio
+
+    interface = gradio.ChatInterface(get_answer)
+    interface.launch()
 
 
 if __name__ == "__main__":
